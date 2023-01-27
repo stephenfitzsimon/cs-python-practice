6c653bba7918413a45f1363024e84bff842535f9
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2023-01-27 15:57:51 -0600

    added menu

--- final_project_1/main.py
+++ final_project_1/main.py
@@ -1,8 +1,22 @@
 from mortgage import Mortgage
 
 def main():
-    mort = Mortgage(0.06, 12, 1000, "test")
-    print(mort)
+    while True:
+        print_menu()
+        user_in = int(input("Enter a selection >"))
+        if user_in == 3:
+            break
+
+def print_menu():
+    menu = '''
+    This is the Mortgage calculator
+
+    Choose one of the following options:
+    1 - Enter new mortgage
+    2 - Compare entered mortgages
+    3 - Exit
+    '''
+    print(menu)
 
 if __name__ == '__main__':
     main()
